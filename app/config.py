ddf234faa8256b3b28c5b8f669e3772bca291a6b
gamzenurmadan <gamzenurmadan@outlook.com>
2024-07-12 16:05:17 +0300

    edited docker-compose

--- app/config.py
+++ app/config.py
@@ -1,5 +1,11 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql+psycopg2://postgres:9876@localhost:5432/star_db')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://{username}:{password}@{host}:{port}/{database}'.format(
+        username=os.environ['RDS_USERNAME'],
+        password=os.environ['RDS_PASSWORD'],
+        host=os.environ['RDS_HOSTNAME'],
+        port=os.environ['RDS_PORT'],
+        database=os.environ['RDS_DB_NAME'],
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
